75f29dc7f2d6855207d309d5b5ad4e1d938aae70
Brian Lambert <brianlambert2408@gmail.com>
2023-01-16 16:18:02 +1100

    Added basic colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,36 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
+
+  const [ theme, setTheme ] = useState("light");
+
+  const toggleTheme =()=>{
+    setTheme(state=> state==="light" ? "dark" : "light");
+  };
+
+  useEffect(()=>{
+    document.documentElement.dataset.theme = theme;
+  },[theme])
 
   return (
     <div className="App">
+      <div className="window1">
+        <div className="w1 bg hi1">text is here <button>+</button></div>
+        <div className="w1 bg hi2">text is here <button>+</button></div>
+        <div className="w1 bg hi3">text is here <button>+</button></div>
+        <h1>text is here</h1>
+        <h2>text is here</h2>
+        text is here
 
+        <h1 className="txt hi1">text is here</h1>
+        <h2 className="txt hi2">text is here</h2>
+        <span className="txt hi3">text is here</span>
+        <br />
+        <input type="text" name="" id="" />
+        <button>OK</button>
+      </div>
+      <button onClick={toggleTheme}>TEST</button>
     </div>
   )
 }
