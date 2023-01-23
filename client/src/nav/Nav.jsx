a6a50db5c0cc2148f478ebbf5c3af94926e0f009
Brian Lambert <brianlambert2408@gmail.com>
2023-01-24 09:30:25 +1100

    New vehicle button

--- client/src/nav/Nav.jsx
+++ client/src/nav/Nav.jsx
@@ -3,6 +3,9 @@ import { toggleTheme } from "../reducers/themeSlice";
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchLogout } from "../reducers/userSlice";
 import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { DASH_PATH } from "../appconstants";
+import { useState } from "react";
 
 export default function Nav(){
 
@@ -10,6 +13,8 @@ export default function Nav(){
     const user = useSelector(state => state.user);
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const [ showDashButtons, setShowDashButtons ] = useState(false);
 
     const handleLogout = async ()=>{
         try {
@@ -19,15 +24,33 @@ export default function Nav(){
         }
     };
 
+    useEffect(()=>{
+
+        history.listen((location) => {
+            setShowDashButtons(state=>location.pathname === DASH_PATH)
+        })
+
+        return ()=>{};
+
+    },[history]);
+
     return(
         <nav className="bodyItem navMain">
             <div>
+                {
+                    showDashButtons 
+                    ? <button onClick={()=>history.push(`${DASH_PATH}/vehicles/new`)} className="btn-hi"><i className="fa fa-plus "></i></button>
+                    : null
+                }
+                
+                <button onClick={()=>history.push(DASH_PATH)}>DASHBOARD</button>
                 { 
                     user.loggedIn 
                     ? <button onClick={handleLogout}><i className="fa fa-sign-out"></i></button>
                     : null
                 }
                 <button onClick={()=>dispatch(toggleTheme())}><i className={`fa ${theme ==="light" ? "fa-moon-o" : "fa-sun-o"}`}></i></button>
+            
             </div>
         </nav>
     );
